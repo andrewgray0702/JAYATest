92cac567231f3eae5f34182e7d063615394f68a8
andrewgray0702 <andrew.gray@midlandu.edu>
2019-05-23 10:53:38 -0500

    -

--- JAYAProject/server.js
+++ JAYAProject/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const weatherRoutes = require('./routes/weather.routes');
 const locationRoutes = require('./routes/location.routes');
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
@@ -9,7 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname+"/dist/JAYAProject"));
 app.use(bodyParser.json());
 
-app.use('/api/weather/', weatherRoutes);
 app.use('/api/location/', locationRoutes);
 
 app.get('*', (req, res) => {
