2dda344e72d60b23a09041f024ac4b8c19a85d0a
andrewgray0702 <andrew.gray@midlandu.edu>
2019-05-23 11:08:52 -0500

    fixed routing

--- JAYAProject/src/app/app-routing.module.ts
+++ JAYAProject/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { GetWeatherComponent } from './components/get-weather/get-weather.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: 'weather', component: GetWeatherComponent},
   {path: 'home', component: HomeComponent},
   {path: '**', redirectTo: 'home'}
 ];
